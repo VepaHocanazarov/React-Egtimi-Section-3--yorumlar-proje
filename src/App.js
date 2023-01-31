904091c19929d5749b3d7c731ee1f9218d836173
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-31 11:29:06 +0300

    Projeyi  Componentlere Ayırma

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import{ faker} from '@faker-js/faker'
+import Comment from './Comment'
 
 const App = () => {
   return (
     <div className="ui comments">
-      <div className="comment">
-        <a className="avatar" href='/'>
-          <img alt='' src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a className="author" href='/'>Vepa Hocanazarov</a>
-          <div className="metadata">
-            <div className="date">2 days ago</div>
-            <div className="rating">
-              <i className="star icon"></i>
-              5 Yildiz
-            </div>
-          </div>
-          <div className="text">
-            Bu bir test mesajdir.
-          </div>
-        </div>
-      </div>
+
+      <Comment/>
+
     </div>
   )
 }
