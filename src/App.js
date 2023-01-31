c704783dce94094ecacc8f56250946a8678bf5b5
Vepa Hocanazarov <vefa00055@gmail.com>
2023-01-31 13:08:16 +0300

    Card.js componenti App.js'e eklendi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Comment from './Comment'
+import Card from './Card'
 
 const App = () => {
   return (
     <div className="ui comments">
+
+      <Card/>
 
       <Comment name = "Vepa Hocanazarov" day = "3" star = "4" comment = "React Egtimi"  />
       <Comment name = "Aygul Cakanova" day = "10" star = "2" comment = "React Egtimi test mesaj" />
